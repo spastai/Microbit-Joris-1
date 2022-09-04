ac470b4990fd05782763ee71037e594a9521db7a
spastai <spastai@gmail.com>
2022-09-04 09:39:24 +0200

    Disco added

--- main.ts
+++ main.ts
@@ -1,29 +1,45 @@
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-    }
-    if (input.buttonIsPressed(Button.AB)) {
-        basic.showLeds(`
-            . . . . .
-            . # . # .
-            . . . . .
-            . . . . .
-            # # # # #
-            `)
-    }
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Red)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Blue)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Cyan)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.White)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Pinkish)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Yellow)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.White)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Pinkish)
+    basic.pause(1000)
+    mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.OFF)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . # . .
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(5000)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
 })
